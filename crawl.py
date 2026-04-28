ef8b180e489ca77654f08fbf651728d1f6290b53
Daniel Soney <daniel.m.soney@student.uts.edu.au>
2026-04-28 22:42:34 +1000

    imported csv files and gcs connector

--- crawl.py
+++ crawl.py
@@ -1,6 +1,14 @@
 import kagglehub
+import shutil
+import os
 
 path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
 
 print("Path to dataset files:", path)
 
+destination = "data/"
+
+for file in os.listdir(path):
+    shutil.copy(os.path.join(path, file), destination)
+
+print("Files copied to data/")
